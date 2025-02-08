18dcc3584e4676c21bcd7c5232d5d68aaf20e56e
sam <samchimaster@gmail.com>
2025-02-08 12:06:38 +0000

    basic sentiment analysis

--- api.py
+++ api.py
@@ -17,19 +17,23 @@ response = requests.request("POST", url, json=payload, headers=headers)
 
 rawData =response.text
 data = json.loads(rawData)
-with open("tweets.json","a+") as file1, open("ids.txt","w+") as file2:
+with open("tweets.json","w+") as file1, open("tweetIds.txt","w+") as file2:
     ids = file2.read()
+    dictList = []
     for tweet in data:
         if (tweet["id"] not in ids):
-            reformatedTweet = {"Handle": tweet["user"]["username"], 
+            reformatedTweet = {
+                    "Handle": tweet["user"]["username"], 
                     "Followers":tweet["user"]["followers_count"],
                     "Likes":tweet["like_count"],
                      "Retweets": tweet["retweet_count"],
                      "Tweet": tweet["text"],
                      "Date Posted": tweet["created_at"]
                      }
-            json.dump(reformatedTweet,file1,indent =1 )
+            dictList.append(reformatedTweet)
             file2.write(tweet["id"]+"\n")
         else:
             print("Already have tweet\n")
+    json.dump(dictList,file1,indent =4 )
     file1.close()
+    file2.close()
