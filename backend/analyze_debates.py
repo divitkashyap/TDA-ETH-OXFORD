178bf8649a348cf99b393ec81769e6754057ab39
sam <samchimaster@gmail.com>
2025-02-08 13:04:46 +0000

    Basic sentiment analysis achieved

--- backend/analyze_debates.py
+++ backend/analyze_debates.py
@@ -1,14 +1,31 @@
 import nltk 
 import json
-
+import spacy 
 from nltk.sentiment import SentimentIntensityAnalyzer
 from nltk import word_tokenize, pos_tag, ne_chunk
-nltk.download('punkt')
-nltk.download('averaged_perceptron_tagger')
-nltk.download('maxent_ne_chunker')
-nltk.download('words')
-nltk.download("vader_lexicon")
+
+try:
+    nltk.data.find('tokenizers/punkt')
+except LookupError:
+    nltk.download('punkt')
+
+try:
+    nltk.data.find('taggers/averaged_perceptron_tagger')
+except LookupError:
+    nltk.download('averaged_perceptron_tagger')
+
+try:
+    nltk.data.find('chunkers/maxent_ne_chunker')
+except LookupError:
+    nltk.download('maxent_ne_chunker')
+
+try:
+    nltk.data.find('corpora/words')
+except LookupError:
+    nltk.download('words')
+
 sia = SentimentIntensityAnalyzer()
+nlp = spacy.load("en_core_web_sm")
 
 def get_sentiment(text):
     sentiment = sia.polarity_scores(text)
@@ -30,7 +47,8 @@ with open("tweets.json","r") as file1:
         tagged = pos_tag(tokens)
         entityRec = ne_chunk(tagged)
         print(f"Tweet: {text} → Sentiment: {get_sentiment(text)}")
-       
-        
+        doc = nlp(text)
+        for ent in doc.ents:
+            print(f"Entity: {ent.text}, Label: {ent.label_}")
                 
 
